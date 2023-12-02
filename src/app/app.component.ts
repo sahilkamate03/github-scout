e4873423489eaeedf83ea308e6c57fd115038f02
sahilkamate03 <45514385+sahilkamate03@users.noreply.github.com>
2023-12-03 02:34:27 +0530

    added pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,86 @@ import { ApiService } from './services/api.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
+
+  repoData: any[] = [];
+  userData: any;
+  githubUsername: string = 'fylein';
+  currentPage: number = 1;
+  maxPage: number = 1;
+  perPage: number = 10;
+  serachInput: any;
+
+  getResults(username: any) {
+    this.githubUsername = username;
+
+    this.getUserDetail(username);
+    this.getRepoDetail(username);
+  }
+
+  getUserDetail(username: any) {
+    // Access the form data here and perform necessary actions
+    // console.log(formData);
+    this.githubUsername = username;
+
+    this.apiService.getUser(username).subscribe((data: any) => {
+      console.log(data); // Explore the data available
+      this.userData = data; // Assign the JSON data to the property
+    });
+  }
+
+  updatePerPage(perPage: number | null) {
+    console.log("Per Page: " + perPage);
+    if (perPage === null) {
+      perPage = this.perPage;
+    }
+    if (perPage >= 1 && perPage <= 100) {
+      this.perPage = perPage;
+      console.log("Per Page: " + this.perPage);
+      this.getRepoDetail(this.githubUsername);
+    } else {
+      console.log("Invalid perPage value. Please enter a value between 1 and 100.");
+    }
+  }
+
+  getRepoDetail(username: any) {
+    // Access the form data here and perform necessary actions
+    // console.log(formData);
+    this.githubUsername = username;
+
+    this.apiService.getUserRepo(username, this.currentPage, this.perPage).subscribe((data: any) => {
+      console.log(data); // Explore the data available
+      this.repoData = data; // Assign the JSON data to the property
+    });
+  }
+
+  updateCurrentPageNumber(pageNumber: number) {
+    this.currentPage = pageNumber;
+    console.log("Current Page Number: " + this.currentPage);
+    this.getRepoDetail(this.githubUsername);
+  }
+
   constructor(
     private apiService: ApiService
-  ) {}
+  ) { }
+
+  async handlePagination() {
+    this.maxPage = await this.apiService.getMaxPage(this.githubUsername, this.perPage) ?? 1;
+  }
 
   ngOnInit() {
-    this.apiService.getUser('johnpapa').subscribe(console.log);
+    this.apiService.getUserRepo('fylein', this.currentPage, this.perPage).subscribe((data: any) => {
+      console.log(data); // Explore the data available
+      this.repoData = data; // Assign the JSON data to the property
+    });
+
+    this.apiService.getUser('fylein').subscribe((data: any) => {
+      console.log(data); // Explore the data available
+      this.userData = data; // Assign the JSON data to the property
+    });
+
+    this.handlePagination();
   }
+
 }
+
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap, throwError } from 'rxjs';
+import { retry, tap, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +14,49 @@ export class ApiService {
   getUser(githubUsername: string) {
     return this.httpClient.get(`https://api.github.com/users/${githubUsername}`);
   }
+
+  getUserRepo(githubUsername: string, page: number, per_page: number) {
+    return this.httpClient.get(`https://api.github.com/users/${githubUsername}/repos`,
+      {
+        params: new HttpParams().set('per_page', per_page).set('page', page)
+      });
+  }
+
+  getPageNumber(linkHeader: string): number | null {
+    const regex = /page=(\d+)>; rel="last"/;
+    const match = linkHeader.match(regex);
+
+    if (match && match[1]) {
+      return parseInt(match[1], 10);
+    }
+
+    return null;
+  }
+
+  getMaxPage(githubUsername: string, per_page: number): Promise<number> {
+    return new Promise((resolve, reject) => {
+      var maxPage = 1;
+      this.httpClient.get(`https://api.github.com/users/${githubUsername}/repos`, {
+        params: new HttpParams().set('per_page', per_page),
+        observe: 'response'
+      }).subscribe(response => {
+        const linkHeader = response.headers.get('Link');
+        const links = linkHeader?.split(',');
+        const prevLink = links?.find(l => l.includes('rel="prev"'));
+        const nextLink = links?.find(l => l.includes('rel="next"'));
+  
+        const pageNumber = this.getPageNumber(linkHeader!);
+        if (pageNumber !== null) {
+          maxPage = pageNumber;
+        }
+      }, error => {
+        reject(error);
+      }, () => {
+        resolve(maxPage);
+      });
+    });
+  }
+  
 
   // implement getRepos method by referring to the documentation. Add proper types for the return type and params 
 }
