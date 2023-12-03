d3c1d80acbf386b22a22dc1980dc054e604b364d
sahilkamate03 <45514385+sahilkamate03@users.noreply.github.com>
2023-12-03 11:24:47 +0530

    added skeletal loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,55 +15,66 @@ export class AppComponent implements OnInit {
   maxPage: number = 1;
   perPage: number = 10;
   serachInput: any;
-  itemsPerPage = [10, 20, 50, 100];
+  isLoading: boolean = true;
   selectedOption = 10;
 
   // Get results for the given username
   getResults(username: any) {
+    this.isLoading = true;
     this.githubUsername = username;
-
     this.getUserDetail(this.githubUsername);
     this.getRepoDetail(this.githubUsername);
+    this.isLoading = false;
   }
 
   // Get user details for the given username
   getUserDetail(username: any) {
+    this.isLoading = true;
+
     this.githubUsername = username;
 
     this.apiService.getUser(username).subscribe((data: any) => {
       console.log(data);
       this.userData = data;
     });
+    this.isLoading = false;
+
   }
 
   // Update the number of items per page
   updatePerPage(perPage: string) {
+    this.isLoading = true;
     var perPageNumber = parseInt(perPage);
     this.currentPage = 1;
     if (perPageNumber >= 1 && perPageNumber <= 100) {
       this.perPage = perPageNumber;
       console.log("Per Page: " + this.perPage);
       this.getRepoDetail(this.githubUsername);
-      this.handlePagination();
     } else {
       console.log("Invalid perPage value. Please enter a value between 1 and 100.");
     }
+    this.isLoading = false;
   }
 
   // Get repository details for the given username
   getRepoDetail(username: any) {
+    this.isLoading = true;
+    this.handlePagination();
     this.githubUsername = username;
     this.apiService.getUserRepo(this.githubUsername, this.currentPage, this.perPage).subscribe((data: any) => {
       console.log(data);
       this.repoData = data;
     });
+    this.isLoading = false;
   }
 
   // Update the current page number
   updateCurrentPageNumber(pageNumber: number) {
+    this.isLoading = true;
     this.currentPage = pageNumber;
     console.log("Current Page Number: " + this.currentPage);
     this.getRepoDetail(this.githubUsername);
+    this.isLoading = false;
   }
 
   constructor(
@@ -76,17 +87,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.apiService.getUserRepo('fylein', this.currentPage, this.perPage).subscribe((data: any) => {
-      console.log(data);
-      this.repoData = data;
-    });
-
-    this.apiService.getUser('fylein').subscribe((data: any) => {
-      console.log(data);
-      this.userData = data;
-    });
-
-    this.handlePagination();
+    this.getResults(this.githubUsername);
   }
 
 }
