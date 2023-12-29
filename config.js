2944683969a4802f087aed6c82c1ff3e71b6372d
sahilkamate03 <45514385+sahilkamate03@users.noreply.github.com>
2023-12-29 10:39:59 +0530

    added config.js for production

---
+++ config.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = './src/app/environment/environments.prod.ts';
+const directoryPath = path.dirname(filePath);
+
+if (!fs.existsSync(directoryPath)) {
+    fs.mkdirSync(directoryPath, { recursive: true });
+}
+
+fs.writeFileSync(filePath, ``);
+
+fs.appendFileSync(filePath, `
+export const environment = {
+        production: true,
+        apiKey: ${process.env.GIHUB_API_KEY},
+};
+`);
+
